4dc37f50e1ef3e53844da27fa620b0c24d744cde
Fernando Camilo <a13233@alunos.ipca.pt>
2018-11-06 19:36:28 +0000

    nothing special

---
+++ Assets/ExampleScripts/AnimatorOverride.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorOverride : MonoBehaviour
+{
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            //Vector3 newPosition = transform.position;
+            //newPosition.y -= animator.GetFloat("Runspeed") * Time.deltaTime;
+            //transform.position = newPosition;
+        }
+    }
+}
